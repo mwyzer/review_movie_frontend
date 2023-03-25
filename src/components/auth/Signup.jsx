12b93d47c89525786549b2361474ab5b7a8a31c8
mwyzer <muhammad.wyzer@gmail.com>
2023-03-26 05:07:40 +0700

    first commit

---
+++ src/components/auth/Signup.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { commonModalClasses } from '../../utils/theme';
+import Container from '../Container';
+import CustomLink from '../CustomLink';
+import FormContainer from '../form/FormContainer';
+import FormInput from '../form/FormInput';
+import Submit from '../form/Submit';
+import Title from '../form/Title';
+
+const validateUserInfo = ({ name, email, password }) => {
+  const isValidEmail = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/;
+  const isValidName =
+    /^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/;
+  const isValidPassword = password.length;
+
+  if (!name.trim()) return { ok: false, error: 'Name is missing!' };
+  if (!isValidName.test(name)) return { ok: false, error: 'Invalid name!' };
+
+  if (!email.trim()) return { ok: false, error: 'Email is missing!' };
+  if (!isValidEmail.test(email)) return { ok: false, error: 'Invalid email!' };
+
+  if (!password.trim()) return { ok: false, error: 'Password is missing!' };
+  if (!isValidPassword < 8)
+    return { ok: false, error: 'Password must be 8 characters long!' };
+
+  return { ok: true };
+};
+
+export default function Signup() {
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { ok, error } = validateUserInfo(userInfo);
+
+    if (!ok) return console.log(error);
+
+    console.log(userInfo);
+  };
+
+  const { name, email, password } = userInfo;
+
+  return (
+    <FormContainer>
+      <Container>
+        <form onSubmit={handleSubmit} className={commonModalClasses + ' w-72'}>
+          <Title>Sign up</Title>
+          <FormInput
+            value={name}
+            onChange={handleChange}
+            label='Name'
+            placeholder='John Doe'
+            name='name'
+          />
+          <FormInput
+            value={email}
+            onChange={handleChange}
+            label='Email'
+            placeholder='john@email.com'
+            name='email'
+          />
+          <FormInput
+            value={password}
+            onChange={handleChange}
+            label='Password'
+            placeholder='********'
+            name='password'
+            type='password'
+          />
+          <Submit value='Sign up' />
+
+          <div className='flex justify-between'>
+            <CustomLink to='/auth/forget-password'>Forget password</CustomLink>
+            <CustomLink to='/auth/signin'>Sign in</CustomLink>
+          </div>
+        </form>
+      </Container>
+    </FormContainer>
+  );
+}
